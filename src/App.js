a4e7f1b417f9ef985628584eda9c95fa68afb71f
ericfruchtman <fruchtmane@yahoo.com>
2021-04-01 15:23:48 -0400

    updating front-end

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ function App() {
   
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
+  const [questions, setQuestions]= useState();
+
+  let apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
   const fetchCategories = async () => {
     console.log('fetching the categories');
-    let res = await fetch('http://localhost:3000/api/v1/categories');
+    /* let res = await fetch('http://localhost:3000/api/v1/categories'); */
+    console.log(`${apiUrl}/api/v1/categories`)
+    let res = await fetch(`${apiUrl}/api/v1/categories`);
     let data = await res.json();
     console.log(data);
     setCategories(data);
@@ -24,9 +29,21 @@ function App() {
     // this code will run whenever the selectedCategory changes
 }, [selectedCategory])
 
+const fetchQuestionsForCategory = async (id) => {
+  console.log('fetch questions for this category id', id);
+  let res = await fetch (`http://localhost:3000/api/v1/categories/${id}/questions`);
+  let data = await res.json();
+  console.log(data); 
+};
+
+const createNewQuestion = async () => {
+  console.log ('create a questions for the category id', selectedCategory)
+};
+
 
   return (
     <>
+
     <div className="grid grid-cols-12">
       <div className={'col-span-full border p-5'}>
         <h1 className={'text-center text-3xl'}>Custom Quiz Generator</h1>
@@ -34,11 +51,14 @@ function App() {
     </div>
     <div className="grid grid-cols-12">
       <div className= {'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
+        <h1 className={'text-center text-3xl'}>Currently selected Category is: {selectedCategory}</h1>
         <ul>
           {categories.map((category, index) => {
             return <li key={index} className={category.Id == 
               selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} 
-              onClick={() => {setSelectedCategory(category.id);
+              onClick={() => {
+                setSelectedCategory(category.id);
+                fetchQuestionsForCategory(category.id)
               }}>
                 {category.name}
               </li>
@@ -48,6 +68,20 @@ function App() {
       
       <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
         <h1 className={'text-center text-3xl'}>blank space</h1>
+
+        <button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>
+        <br />
+        <br />
+
+        <ul>
+          {questions && questions.map((question) => {
+            return <li key={question.id}>
+              {question.questionTxt}
+              {question.Answers.length >0 && <span>- <span>{question.Answers.length}
+              </span></span>}
+            </li>
+          })}
+        </ul>
       </div>
     </div>
     </>
