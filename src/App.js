3b3d23e274832eedca88d60d5de99e8faee98f66
ericfruchtman <fruchtmane@yahoo.com>
2021-04-05 10:11:24 -0400

    checking in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from "react";
 import {Button, List, Collapse} from 'antd';
@@ -9,15 +8,15 @@ function App() {
   
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
-  const [questions, setQuestions]= useState();
+  const [questions, setQuestions]= useState([]);
+  const [selectedQuestion, setSelectedQuestion]= useState();
   const [questionTxt, setQuestionTxt]= useState('');
+  const [answers, setAnswers]= useState([]);
+  const [answerTxt, setAnswerTxt]= useState('');
 
   let apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
   const fetchCategories = async () => {
-    console.log('fetching the categories');
-    /* let res = await fetch('http://localhost:3000/api/v1/categories'); */
-    console.log(`${apiUrl}/api/v1/categories`)
     let res = await fetch(`${apiUrl}/api/v1/categories`);
     let data = await res.json();
     console.log(data);
@@ -28,22 +27,36 @@ function App() {
     //this code runs once on component mount
     fetchCategories()
   }, [])
-
+  
   useEffect(() => {
-    // this code will run whenever the selectedCategory changes
-}, [selectedCategory])
+    fetchQuestionsForCategory();
+  }, [selectedCategory])
+  
+  useEffect(() => {
+    fetchAnswers();
+  }, [selectedQuestion])
 
 const fetchQuestionsForCategory = async (id) => {
-  console.log('fetch questions for this category id', id);
-  let res = await fetch (`http://localhost:3000/api/v1/categories/${id}/questions`);
+  console.log(id);
+  let res = await fetch (`${apiUrl}/api/v1/categories/${id}/questions`);
   let data = await res.json();
   console.log(data);
   setQuestions(data); 
 };
 
+
+const fetchAnswers = async (id) => {
+  console.log(id);
+    let res = await fetch(`${apiUrl}/api/v1/categories/:categoryId/questions/${id}/answers`);
+    let data = await res.json();
+    console.log(data);
+    setAnswers(data);
+  };
+
+
 /* Make Post request to create a question */
 const createNewQuestion = async () => {
-  console.log ('create a questions for the category id', selectedCategory)
+  console.log (selectedCategory)
   let res = await fetch (`${apiUrl}/api/v1/categories/${selectedCategory}/questions`, {
     method: 'POST',
     headers: {
@@ -56,6 +69,16 @@ const createNewQuestion = async () => {
 };
 
 const createANewAnswer = async () => {
+  console.log(selectedQuestion)
+  let res = await fetch(`${apiUrl}/api/v1/categories/:categoryId/questions/${selectedQuestion}/answers`, {
+    method: 'POST',
+    headers: {},
+    body: JSON.stringify({answerTxt: answerTxt})
+});
+fetchAnswers(selectedQuestion);
+fetchQuestionsForCategory();
+setAnswerTxt('')
+};
         // you will need something called selectedQuestion to keep a track of the question that has been selected
       // a state variable to store the answer text that the user types in
 
@@ -64,19 +87,77 @@ const createANewAnswer = async () => {
       // 2. Once the call is successful
       // 3. Fetch the questions for a category again (reload the questions)
       // 4. done!
-};
-
 
   return (
     <>
-
     <div className="grid grid-cols-12">
       <div className={'col-span-full border p-5'}>
         <h1 className={'text-center text-3xl'}>Custom Quiz Generator</h1>
       </div>
     </div>
     <div className="grid grid-cols-12">
-      <div className= {'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
+              {/* Sidebar */}
+        <div className={"col-span-full md:col-span-3 lg:col-span-2 border p-4"}>
+          <ul>
+            {categories.map((category, index) => {
+              return <li key={index} className={category.id == selectedCategory ? 
+                "rounded border my-2 p-4 cursor-pointer bg-blue-500 text-white font-bold" : "rounded border my-2 p-4 cursor-pointer"} 
+                onClick={() => {
+                  setSelectedCategory(category.id);
+                  setSelectedQuestion('');
+                }}>
+                {category.name}
+              </li>
+            })}
+          </ul>
+        </div>
+
+        {/* Results */}
+        <div className={"col-span-full md:col-span-9 lg:col-span-10 border p-4"}>
+          <ul>
+            <div className={"grid grid-cols-12 gap-4"}>
+              
+              {/* Question Input Bar */}
+              {selectedCategory && <input value={questionTxt} onChange={(event) => {
+                setQuestionTxt(event.currentTarget.value);
+                }} type="text" className={'col-span-full md:col-span-9 lg:col-span-10 border w-full rounded border-gray-300 p-2'}/>}
+                {selectedCategory && <span className={'col-span-full md:col-span-3 lg:col-span-2 bg-green-600 cursor-pointer rounded text-center text-white text-xl md:text-lg p-2'} onClick={createNewQuestion}>New Question</span>}
+            </div>
+            <br/>            
+            
+            {/* Responses & Answer Input Bar */}
+            {questions.map((question, index) => {
+              return <div>
+                <li key={index} className={question.id == selectedQuestion ? 
+                "border my-2 p-4 bg-gray-300 font-bold" : "border my-2 p-4 cursor-pointer"} 
+                onClick={() => {
+                setSelectedQuestion(question.id);
+                }}>
+                  {question.questionTxt}
+                  {question.Answers.length > 0 && <span> - Number of Answers: {question.Answers.length}</span>}
+                                                      
+                  {answers.map((answer, id) => {
+                    return answer.questionId == question.id && <div key={id} className={"rounded border p-4 mt-4 bg-white font-normal"}>
+                      {answer.answerTxt}
+                    </div>
+                  })}
+               </li>
+               {selectedQuestion == question.id && 
+               <div className={"bg-gray-300 p-4 border"}>
+                 <input value={answerTxt} onChange={(event) => {
+                   setAnswerTxt(event.currentTarget.value);
+                   }} type="text" className={'rounded border p-2 mr-5 w-3/4'}/>
+                  <button className={"col-span-full md:col-span-3 lg:col-span-2 bg-green-600 cursor-pointer rounded text-center text-white text-xl md:text-lg p-2"} 
+                  onClick={createANewAnswer}>Add Answer</button>
+                </div>}
+                   
+              </div>
+
+            })}
+            </ul>
+
+
+        
         {/* <h1 className={'text-center text-3xl'}>Currently selected Category is: {selectedCategory}</h1>
         <ul>
           {categories.map((category, index) => {
@@ -91,7 +172,7 @@ const createANewAnswer = async () => {
           })}
         </ul>  */}
 
-        <List
+        {/* <List
         size= "large"
         header={<div className={'font-bold'}>Categories</div>}
         bordered
@@ -107,18 +188,18 @@ const createANewAnswer = async () => {
         />
       </div>
       
-      <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
+      <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}> */}
 
 {/*<button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>*/}
 
-{selectedCategory && <div>
+{/* {selectedCategory && <div>
     <input value={questionTxt} onChange={(ev) => {
         setQuestionTxt(ev.currentTarget.value);
     }} type="text" className={'border p-1 mr-5 w-2/3'}/>
     <Button type={'primary'} onClick={createNewQuestion}>Create new question</Button>
     <br/>
     <br/>
-</div>}
+</div>} */}
 
 {/*<ul>*/}
 {/*    {questions && questions.map((question) => {*/}
@@ -129,7 +210,7 @@ const createANewAnswer = async () => {
 {/*    })}*/}
 {/*</ul>*/}
 
-{selectedCategory && <Collapse accordion>
+{/* {selectedCategory && <Collapse accordion>
     {questions && questions.map((question, index) => {
         return <Panel header={question.questionTxt} key={index}>
             <List
@@ -151,7 +232,7 @@ const createANewAnswer = async () => {
             />
         </Panel>
     })}
-</Collapse>}
+</Collapse>} */}
 
 {!selectedCategory && <h1 className={'text-center text-xl uppercase tracking-wider text-blue-500'}>Select a category to get started</h1>}
 {/*{questions && <p>{JSON.stringify(questions)}</p>}*/}
